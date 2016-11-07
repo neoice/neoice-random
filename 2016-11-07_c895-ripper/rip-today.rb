f718ca7b2b8b102eb8eeb29836b1a4d21f1e505f
neoice <neoice@neoice.net>
2016-11-07 08:58:05 -0800

    quick improvement for per-day directories

--- 2016-11-07_c895-ripper/rip-today.rb
+++ 2016-11-07_c895-ripper/rip-today.rb
@@ -16,7 +16,8 @@ days = [ day ]
 target_dir = "/tank/media/music/c895"
 
 # morning show
-hours = ["13", "14", "15", "16"]
+# TODO: this varies with DST!!!
+hours = ["14", "15", "16", "17"]
 dj = "drew-bailey"
 
 # vortex
@@ -27,12 +28,17 @@ dj = "drew-bailey"
 minutes = ["0000", "1500", "3000", "4500"]
 
 # let's print some info first
+# directory for today too
 day_str = now.strftime('%A %b %d')
+day_dir = now.strftime("%Y-%m-%d_%A").downcase
 
 puts "[!] today is #{day_str}"
+
 puts "[!] saving files to #{target_dir}"
+puts "[+] making directory #{day_dir}"
 
 sleep 2
+Dir.mkdir "#{target_dir}/#{day_dir}"
 
 days.each do |x|
   puts "[+] ripping day #{x}"
@@ -43,7 +49,7 @@ days.each do |x|
       puts "[-] downloading file #{filename}..."
 
       # Marion, don't look at it.
-      `curl 'https://s3-us-west-1.amazonaws.com/rfa-archive-dev/#{filename}' --silent -H 'Accept-Encoding: identity;q=1, *;q=0' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36' -H 'Accept: */*' -H 'Referer: http://www.radiofreeamerica.com/dj/#{dj}' -H 'Connection: keep-alive' -H 'Range: bytes=0-' --compressed > #{target_dir}/#{filename}`
+      `curl 'https://s3-us-west-1.amazonaws.com/rfa-archive-dev/#{filename}' --silent -H 'Accept-Encoding: identity;q=1, *;q=0' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36' -H 'Accept: */*' -H 'Referer: http://www.radiofreeamerica.com/dj/#{dj}' -H 'Connection: keep-alive' -H 'Range: bytes=0-' --compressed > #{target_dir}/#{day_dir}/#{filename}`
     end
   end
 end
